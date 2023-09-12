28cade18411ca23ad6877f49a0b16e47fbd13de9
Otávio Henrique <99184583+Likrevo@users.noreply.github.com>
2023-09-11 21:31:29 -0300

    Quarto Exercício.

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -6,3 +6,7 @@ SELECT nome FROM autores WHERE nascimento <= '1900-01-01';
 -- Third
 SELECT titulo, autor_id, nome FROM livros l 
 JOIN autores a ON a.id = l.autor_id WHERE a.nome = 'J.K. Rowling';
+
+-- Fourth
+SELECT aluno_id, nome, curso FROM alunos a
+LEFT JOIN matriculas m ON m.id  = a.id WHERE curso = "Engenharia de Software";
