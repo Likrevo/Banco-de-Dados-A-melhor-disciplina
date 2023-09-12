a2fed35f1af76cce64a30b82eeedf75974fc156b
Otávio Henrique <99184583+Likrevo@users.noreply.github.com>
2023-09-11 21:28:24 -0300

    Terceiro Exercício.

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -2,3 +2,7 @@ SELECT titulo FROM livros;
 
 -- Second
 SELECT nome FROM autores WHERE nascimento <= '1900-01-01';
+
+-- Third
+SELECT titulo, autor_id, nome FROM livros l 
+JOIN autores a ON a.id = l.autor_id WHERE a.nome = 'J.K. Rowling';
