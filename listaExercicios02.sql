b08aa06ea479df20deffb02bee176c7e48b3129f
Otávio Henrique <99184583+Likrevo@users.noreply.github.com>
2023-09-25 23:53:26 -0300

    EX03

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -22,3 +22,16 @@ DELIMITER ;
 CALL sp_LivrosPorCategoria("Romance");
 CALL sp_LivrosPorCategoria("Autoajuda");
 CALL sp_LivrosPorCategoria("Ficção Científica");
+
+-- Tres
+DELIMITER //
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN p_Categoria VARCHAR(100))
+BEGIN
+    SELECT Categoria_ID AS Id_Da_Categoria, COUNT(*) AS Quantidade_De_Livros FROM Livro 
+    WHERE Categoria_ID = (SELECT Categoria_ID FROM Categoria WHERE Nome = p_categoria) 
+    GROUP BY Categoria_ID ORDER BY COUNT(Categoria_ID);
+END
+//
+
+DELIMITER ;
+CALL sp_ContarLivrosPorCategoria("Autoajuda");
