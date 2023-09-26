9de9348cc3018a04b2e7440beeaa354ebc035eac
Otávio Henrique <99184583+Likrevo@users.noreply.github.com>
2023-09-25 22:11:21 -0300

    EX02

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -8,3 +8,17 @@ END;
 
 DELIMITER ;
 CALL sp_ListarAutores();
+
+-- Dois
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN p_Categoria VARCHAR(100))
+BEGIN
+    SELECT titulo FROM Livro 
+    WHERE Categoria_ID = (SELECT Categoria_ID FROM Categoria 
+    WHERE Nome = p_Categoria);
+END
+//
+DELIMITER ;
+CALL sp_LivrosPorCategoria("Romance");
+CALL sp_LivrosPorCategoria("Autoajuda");
+CALL sp_LivrosPorCategoria("Ficção Científica");
