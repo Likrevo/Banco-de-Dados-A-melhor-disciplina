9869e7a9df65f8313d25bcc26ffb5c442a36bfe9
Otávio Henrique <99184583+Likrevo@users.noreply.github.com>
2023-10-26 19:07:23 -0300

    Exercício 2.

--- 4º Bimestre/exerciciosDeFixacao20231026.sql
+++ 4º Bimestre/exerciciosDeFixacao20231026.sql
@@ -3,3 +3,9 @@ CREATE TRIGGER trg_depois_inserir_clientes
 AFTER INSERT ON Clientes
 FOR EACH ROW INSERT INTO Auditoria(mensagem)
 VALUES("Novo cliente adicionado");
+
+--EX02
+CREATE TRIGGER trg_antes_excluir_clientes 
+BEFORE DELETE ON Clientes
+FOR EACH ROW INSERT INTO Auditoria(mensagem)
+VALUES("Cliente excluido");
