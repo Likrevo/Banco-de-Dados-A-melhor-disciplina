7b647ffa8f17415acf81dc2e9040f6687f335a09
Otávio Henrique <99184583+Likrevo@users.noreply.github.com>
2023-10-26 19:07:46 -0300

    Exercício 3.

--- 4º Bimestre/exerciciosDeFixacao20231026.sql
+++ 4º Bimestre/exerciciosDeFixacao20231026.sql
@@ -9,3 +9,9 @@ CREATE TRIGGER trg_antes_excluir_clientes
 BEFORE DELETE ON Clientes
 FOR EACH ROW INSERT INTO Auditoria(mensagem)
 VALUES("Cliente excluido");
+
+-- EX03
+CREATE TRIGGER trg_depois_nomes_clientes 
+AFTER UPDATE ON Clientes
+FOR EACH ROW INSERT INTO Auditoria(mensagem)
+VALUES(CONCAT("Nome de cliente modificado. Nome antigo: ", OLD.nome, ", novo nome: ", New.nome));
